47cca4412aced28a59ee8c0a0e370fab74235d9d
TarcisioCardosoJunior <tarcisio.junior@hbsis.com.br>
2021-09-18 10:29:03 -0300

    add class

--- Modulo-2/Aula2-ClassesHerança/Exercicios/exerc_1.py
+++ Modulo-2/Aula2-ClassesHerança/Exercicios/exerc_1.py
@@ -11,26 +11,3 @@ Junior é um cliente normal
 Ana é um cliente Vip
 
 """
-
-class CadastroPessoa:
-    def __init__(self,nome, idade, vip=False):
-        self.vip = vip
-        self.nome = nome
-        self.idade = idade
-
-    def tipo_de_cliente(self):
-        if self.vip:
-            print(f"{self.nome} é um cliente Vip")
-            return
-        print(f"{self.nome} é um cliente normal")
-
-
-class Cliente(CadastroPessoa):
-    pass
-
-
-cliente = Cliente(nome="Junior", idade=15, vip=False)
-cliente_vip = Cliente(nome="Ana", idade=25, vip=True)
-
-cliente.tipo_de_cliente()
-cliente_vip.tipo_de_cliente()
